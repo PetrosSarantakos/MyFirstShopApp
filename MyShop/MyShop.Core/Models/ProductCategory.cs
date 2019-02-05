acd2fcf0c3c4c89dd9b4006bd2ab8b51888ad186
Petros Sarantakos <sarntakos.petros@gmail.com>
2019-02-05 21:25:31 +0200

    Impemented Generic Repository

--- MyShop/MyShop.Core/Models/ProductCategory.cs
+++ MyShop/MyShop.Core/Models/ProductCategory.cs
@@ -7,15 +7,9 @@ using System.Threading.Tasks;
 
 namespace MyShop.Core.Models
 {
-    public class ProductCategory
+    public class ProductCategory:BaseEntity
     {
-        public string Id { get; set; }
         [DisplayName("Category")]
         public string Category { get; set; }
-
-        public ProductCategory()
-        {
-            this.Id = Guid.NewGuid().ToString();
-        }
     }
 }
--- MyShop/MyShop.WebUI/Controllers/ProductManagerController.cs
+++ MyShop/MyShop.WebUI/Controllers/ProductManagerController.cs
@@ -11,12 +11,12 @@ namespace MyShop.WebUI.Controllers
 {
     public class ProductManagerController : Controller
     {
-        ProductRepository context;
-        ProductCategoryRepository productCategories;
+        InMemoryRepository<Product> context;
+        InMemoryRepository<ProductCategory> productCategories;
         public ProductManagerController()
         {
-            context = new ProductRepository();
-            productCategories = new ProductCategoryRepository();
+            context = new InMemoryRepository<Product>();
+            productCategories = new InMemoryRepository<ProductCategory>();
         }
         // GET: ProductManager
         public ActionResult Index()
@@ -27,7 +27,6 @@ namespace MyShop.WebUI.Controllers
 
         public ActionResult Create()
         {
-
             ProductManagerVM vm = new ProductManagerVM();
             vm.Product = new Product();
             vm.ProductCategories = productCategories.Collection();
