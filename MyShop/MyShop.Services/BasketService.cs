6852a9437533094602631c20663c41b7d224765f
Petros Sarantakos <sarantakos.petros@gmail.com>
2021-01-10 03:46:46 +0200

    Branch initial commit

---
+++ MyShop/MyShop.Services/BasketService.cs
@@ -0,0 +1,16 @@
+﻿using MyShop.Core.Contracts;
+using MyShop.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShop.Services
+{
+    public class BasketService
+    {
+        IRepository<Product> productContext;
+        IRepository<Basket> basketContext;
+    }
+}
